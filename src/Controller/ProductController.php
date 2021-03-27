2ae5b666b55756d6cd07687f0ba8691bd1fe5090
mmcomp <m.mirsamie@gmail.com>
2021-03-27 14:59:12 +0430

    start symfony

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Product;
+use App\Form\ProductType;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="products")
+     */
+    public function index(): Response
+    {
+        $products = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * @Route("/product/add")
+    */
+    public function new(Request $request): Response
+    {
+        $product = new Product();
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+
+            $imagePath = $form['image_path']->getData();
+            $extension = $imagePath->guessExtension();
+            $webPath = $this->getParameter('kernel.project_dir') . '/public/img/';
+            $fileName = time() . '.' . $extension;
+            $imagePath->move($webPath, $fileName);
+            $product->setImagePath('/img/' . $fileName);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('product/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
